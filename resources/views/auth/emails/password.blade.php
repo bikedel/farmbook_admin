c5553046b08787f6a2e049d37dfb68d74b040909
Bike Del <home@Homes-iMac.local>
2016-02-17 22:28:08 +0200

    spelling mistake

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,5 +1,5 @@
 
-<h1>Welcomt to Farmbooks</h1>
+<h1>Welcome to Farmbooks</h1>
 
 <img src="http://www.proteadb.co.za/farmbook_admin/storage/imports/farmbook.jpeg" alt="Farmbooks" style="width:600px;height:228px;">
 Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
