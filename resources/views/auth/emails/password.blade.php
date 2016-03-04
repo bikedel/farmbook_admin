eaea15ba87e9c0991ba269e1412e2ca64ec115ce
Bike Del <home@Homes-iMac.local>
2016-03-04 13:46:29 +0200

    added br

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,5 +1,5 @@
 
 <h1>ProteaDB</h1>
 
-<img src="http://www.proteadb.co.za/farmbooks/storage/imports/farmbook.jpeg" alt="Farmbooks" style="width:600px;height:228px;">
+<img src="http://www.proteadb.co.za/farmbooks/storage/imports/farmbook.jpeg" alt="Farmbooks" style="width:600px;height:228px;"><br>
 Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
