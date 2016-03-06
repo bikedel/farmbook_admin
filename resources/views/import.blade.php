e6f63f0b1215deadaae41ef90ce2df67bf13b22d
Bike Del <home@Homes-iMac.local>
2016-03-06 12:00:12 +0200

    import changes

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -75,7 +75,7 @@ input[ type=text ]{
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-primary">
-				<div class="panel-heading"> Import 
+				<div class="panel-heading"> Import from SAPTG
 				</div>
 				
 
@@ -105,6 +105,30 @@ input[ type=text ]{
 						{!! Form::submit('Import',  array('class'=>'btn btn-info ')) !!}
 					</div>
 					{!! Form::close() !!}
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary">
+				<div class="panel-heading">Import from Access
+				</div>
+				<div class="panel-body">
+
+					@if ( Session::has('flash_message') )
+
+					<div class="row alert {{ Session::get('flash_type') }} ">
+						<button type="button" class="form-group btn btn-info close" data-dismiss="alert" aria-hidden="true">&times;</button>
+						<p>{{ Session::get('flash_message') }}</p>
+					</div>
+
+					@endif
+
 					{!! Form::open([ 'url' => '/', 'method' => 'get', 'files' => 'true']) !!}
 
 					<div class="form-group">
@@ -137,14 +161,50 @@ input[ type=text ]{
 	</div>
 </div>
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary">
+				<div class="panel-heading"> Create Databse
+				</div>
+				<div class="panel-body">
 
+					@if ( Session::has('flash_message') )
+
+					<div class="row alert {{ Session::get('flash_type') }} ">
+						<button type="button" class="form-group btn btn-info close" data-dismiss="alert" aria-hidden="true">&times;</button>
+						<p>{{ Session::get('flash_message') }}</p>
+					</div>
+
+					@endif
+
+					{!! Form::open([ 'url' => 'createdatabase', 'method' => 'post', 'files' => 'true']) !!}
+					<div class="form-group">
+                        {{ Form::label('text','Suburb Name ',array('id'=>'','class'=>'')) }}
+						{{ Form::text('database') }}
+					</div>
+
+					<div class="form-group">
+						{{ Form::label('text','Select Type ',array('id'=>'','class'=>'')) }}
+						{!! Form::select('type', $type,null, ['class'=> 'form-control input-sm', 'id'=>'type']) !!}
+					</div>
+					<div class="form-group">	
+						
+						{!! Form::submit('Create',  array('class'=>'btn btn-danger ')) !!}
+					</div>
+					{!! Form::close() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-primary">
-				<div class="panel-heading"> Delete
+				<div class="panel-heading"> Delete Database
 				</div>
 				<div class="panel-body">
 
