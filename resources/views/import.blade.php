2679102d8e31cb10ed0b7383f8ca021fbe421162
Bike Del <home@Homes-iMac.local>
2016-03-24 14:08:02 +0200

    disable update

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -140,7 +140,7 @@ input[ type=text ]{
 
 					<div class="form-group">
 
-						{!! Form::submit('Update',  array('class'=>'btn btn-success ')) !!}
+						{!! Form::submit('Update',  array('class'=>'btn btn-success disabled')) !!}
 					</div>
 					{!! Form::close() !!}
 				</div>
