0fa54dcb5da84db0954f1dc0fa30061b67233d31
Bike Del <home@Homes-iMac.local>
2016-05-12 18:29:25 +0200

    update FH - delete old reg from updates table

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -141,6 +141,14 @@ class UpdateController extends Controller
 
         File::put($logfilename, '');
 
+        // check latest regDate in properties and delete old updates
+        $propDate = Property::on($database)->orderBy('dtmRegDate', 'desc')->first();
+
+        Update::on($database)->where('dtmRegDate', '<=', $propDate['dtmRegDate'])->delete();
+
+        $echo = "last dtmRegDate in properties = " . $propDate['dtmRegDate'];
+        File::append($logfilename, $echo . "\r\n");
+
         // get all update records
         //
         $updates = Update::on($database)->orderBy('strKey')->orderBy(DB::raw("STR_TO_DATE(dtmRegDate, '%Y-%m-%d')"))->get();
